1bdadd24cf7f90e05ccbeeb437bae67a0c6d48b8
Mark <luckyzhigang@mail.com>
2013-12-24 11:54:39 +0800

    use angularUI-bootstrap

--- app/scripts/controllers/Question.js
+++ app/scripts/controllers/Question.js
@@ -2,5 +2,30 @@
 
 angular.module('playCppApp')
     .controller('QuestionCtrl', ['$scope', 'Question', function ($scope, Question) {
-        $scope.question = Question.get({id: '52b710f8d3016112a165cf34'})
+        var questions=[]
+            , index = 0;
+        Question.query(function (data) {
+            questions = data
+            $scope.question = questions[index]
+        })
+
+        $scope.pre = function () {
+            $scope.question = questions[--index]
+        }
+        $scope.next = function () {
+            $scope.question = questions[++index]
+        }
+
+        $scope.isFirst = function () {
+            return questions.length==0 || index == 0
+        }
+        $scope.isLast = function () {
+            return questions.length==0 || (index == (questions.length - 1))
+        }
+
+        $scope.select =function(answer){
+            alert(answer)
+            //alert($scope.question.answers[answer].isRight)
+        }
+
     }])
